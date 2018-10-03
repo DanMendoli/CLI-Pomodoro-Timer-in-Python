f64c6d451a7f7d973273586804bf6154ba430ef9
Danilo Mendonça <danilo.mo@live.com>
2018-10-03 19:58:12 -0300

    Initial - Add main and pomoTimer functions

---
+++ main.py
@@ -0,0 +1,50 @@
+import time
+import pyaudio
+import os
+
+
+pomo_min = 0
+pomo_sec = 0
+pomo_count = 0
+
+
+def pomoTimer():
+    global pomo_sec
+    global pomo_min
+
+    for pomo_sec in range(59, -1, -1):
+        if pomo_min >= 0:
+            if pomo_sec < 10:
+                os.system("clear")
+                print("{}:0{}".format(pomo_min, pomo_sec))
+                time.sleep(1)
+            else:
+                os.system("clear")
+                print("{}:{}".format(pomo_min, pomo_sec))
+                time.sleep(1)
+
+            if pomo_sec == 0:
+                pomo_min = pomo_min - 1
+                pomoTimer()
+
+
+def main():
+    global pomo_sec
+    global pomo_min
+    global pomo_count
+
+    os.system("clear")
+
+
+choose = int(input('''
+-- Escolha uma opção --
+[1] Pomodoro
+[2] Pausa curta
+[3] Pausa longa
+ '''))
+if choose == 1:
+    pomo_sec = 59
+    pomo_min = 24
+    pomoTimer()
+
+main()
