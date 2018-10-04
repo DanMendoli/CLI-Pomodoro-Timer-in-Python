2519f093333c6ac2f2acf1f6b6f3b71bdaed1339
Danilo Mendonça <danilo.mo@live.com>
2018-10-04 02:02:58 -0300

    Add short pause function and alarm sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
-import pyaudio
 import os
+from playsound import playsound
 
 
 pomo_min = 0
@@ -28,6 +28,26 @@ def pomoTimer():
                 pomoTimer()
 
 
+def shortPause():
+    global pomo_sec
+    global pomo_min
+
+    for pomo_sec in range(59, -1, -1):
+        if pomo_min >= 0:
+            if pomo_sec < 10:
+                os.system("clear")
+                print("{}:0{}".format(pomo_min, pomo_sec))
+                time.sleep(1)
+            else:
+                os.system("clear")
+                print("{}:{}".format(pomo_min, pomo_sec))
+                time.sleep(1)
+
+            if pomo_sec == 0:
+                pomo_min = pomo_min - 1
+                shortPause()
+
+
 def main():
     global pomo_sec
     global pomo_min
@@ -43,12 +63,23 @@ choose = int(input('''
 [3] Pausa longa
  '''))
 if choose == 1:
-    pomo_min = 24
-    pomoTimer()
+    if pomo_count == 4:
+        pomo_count = 0
+        '''Você já realizou quatro pomodoros. Recomenda-se uma pausa longa, de 15 ou 30 minutos.
+        Deseja fazer uma pausa longa?
+        [s] - sim
+        [n] - não'''
+    else:
+        pomo_min = 24
+        pomoTimer()
+        playsound("analog-watch-alarm_daniel-simion.wav")
+        pomo_count = pomo_count + 1
+
 
 if choose == 2:
     pomo_min = 4
-    pomoTimer()
+    shortPause()
+    playsound("analog-watch-alarm_daniel-simion.wav")
 
 if choose == 3:
     pomo_min = 29
